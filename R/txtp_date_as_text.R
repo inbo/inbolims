0cf4dff68895a774955917855c3e79cac5698584
VERSCHELDE <pieter.verschelde@inbo.be>
2024-06-17 15:42:28 +0200

    Toevoegen functionaliteit texture_parsing met 1 staal (ander formaat) + 1 script per functie voor texture parsing scripts (beginnend met txtp_) + aanpassen documentatie om R CMD CHECK zonder NOTE te kunnen lopen

---
+++ R/txtp_date_as_text.R
@@ -0,0 +1,18 @@
+
+###################
+
+#' Converteer datumvector of POSIXct vector naar text in formaat yyyy-mm-dd
+#'
+#' @param x vector met datums (character)
+#' @param na_result resultaat indien geen geldige datum
+#'
+#' @return character vector with datums
+#' @export
+#'
+date_as_text <- function(x, na_result = "NODATE") {
+  x[is.na(x) | substring(x, 1, 2) != "20"] <- NA
+  x <- paste0(substring(x, 1, 10))
+  x[is.na(x)] <- na_result
+  x
+}
+
